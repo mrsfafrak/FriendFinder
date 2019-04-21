53316b0c34f1d416fd095334725b1564c8437ad2
Tori <victoria.fafrak@gmail.com>
2019-04-20 21:44:04 -0400

    more setup

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # FriendFinder
+
+(https://friend-finder-fsf.herokuapp.com/). 
+
+### Instructions
+
+1. Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
+
+
+3. Your `htmlRoutes.js` file should include two routes:
+
+   * A GET Route to `/survey` which should display the survey page.
+   * A default, catch-all route that leads to `home.html` which displays the home page.
+
+4. Your `apiRoutes.js` file should contain two routes:
+
+   * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+   * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+
+6. Determine the user's most compatible friend using the following as a guide:
+
+   * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+   * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+     * Example:
+       * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+       * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+       * Total Difference: **2 + 1 + 2 =** **_5_**
+   * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+   * The closest match will be the user with the least amount of difference.
+
+7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+   * The modal should display both the name and picture of the closest match.
+
+
+### Hosting on Heroku
+
+Now that we have a backend to our applications, we use Heroku for hosting. Please note that while **Heroku is free**, it will request credit card information if you have more than 5 applications at a time or are adding a database.
+
+Please see [Heroku’s Account Verification Information](https://devcenter.heroku.com/articles/account-verification) for more details.
+
+See the [Supplemental Heroku Deployment Guide](../../03-Supplemental/HerokuGuide.md) for in-detail deployment instructions.
+
+- - -
+
---
+++ app/data/friends.js
@@ -0,0 +1,194 @@
+[
+    {
+        "name": "Doug",
+        "photo": "https://www.harley-davidson.com/content/dam/h-d/images/motorcycles/my19/street/street-500/details/dom/19-street-harley-davidson-street-500-xg500-thumb.jpg?impolicy=myresize&rw=896",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Tim",
+        "photo": "https://ih1.redbubble.net/image.473017718.0760/ap,550x550,12x16,1,transparent,t.u3.png",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Greg",
+        "photo": "https://geology.com/world/china-map.gif",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Gabby",
+        "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-puppy-portrait-royalty-free-image-512366437-1537297563.jpg?crop=1.00xw:0.753xh;0,0.170xh&resize=480:*",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Nicole",
+        "photo": "https://img.purch.com/h/1000/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA1Mi8zNjUvb3JpZ2luYWwvcmVkLXdoaXRlLXdpbmUtMTIwMTExLmpwZw==",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Nitza",
+        "photo": "https://images.pexels.com/photos/459522/pexels-photo-459522.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Facundo",
+        "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--M4yNO3Ve--/c_scale,f_auto,fl_progressive,q_80,w_800/ikjzvdwuxv7zvcdxfcav.jpg",
+        "scores": [
+            "3",
+            "2",
+            "1",
+            "4",
+            "4",
+            "4",
+            "4",
+            "2",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Ron",
+        "photo": "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/vdat/submodels/porsche_718-cayman_porsche-718-cayman_2019-1539099156262.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "4"
+        ]
+    },
+    {
+        "name": "Igor",
+        "photo": "http://euromaidanpress.com/wp-content/uploads/2018/08/Belarus-Ukraine-Russia-1.png",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "4"
+        ]
+    },
+    {
+        "name": "Gabriel",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/1/19/Marvel_Universe_%28Civil_War%29.jpg/220px-Marvel_Universe_%28Civil_War%29.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "4"
+        ]
+    },
+    {
+        "name": "Bryan",
+        "photo": "https://image.spreadshirtmedia.com/image-server/v1/compositions/105993955/views/1,width=650,height=650,appearanceId=63,version=1547213188/team-ramrod.jpg",
+        "scores": [
+            "5",
+            "3",
+            "5",
+            "2",
+            "3",
+            "3",
+            "4",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Lance",
+        "photo": "http://www.brainscape.com/blog/wp-content/uploads/2015/06/French.jpg",
+        "scores": [
+            "3",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    }
+]
